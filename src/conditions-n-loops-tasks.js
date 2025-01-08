52b1ad61b33aa5ca19a5cfcdb2fdbcd219cfcb8b
Anna Drobysh <ann.drobysh@yandex.by>
2025-01-08 14:13:52 +0100

    feat: implement isPalindrome function

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -230,8 +230,15 @@ function convertNumberToString(numberStr) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  let revertedStr = '';
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    revertedStr += str[i];
+  }
+  if (str === revertedStr) {
+    return true;
+  }
+  return false;
 }
 
 /**
